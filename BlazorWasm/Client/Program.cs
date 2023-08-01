df541382091b3eb1e24c47b9fe112be16bc57105
Jan Janoušek <janousek@ocel.cz>
2023-08-01 11:03:06 +0200

    Add project files.

---
+++ BlazorWasm/Client/Program.cs
@@ -0,0 +1,24 @@
+using BlazorWasm.Client;
+using BlazorWasm.Client.Pages;
+using Flurl.Http.Configuration;
+using Flurl.Http;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddSingleton<IFlurlClientFactory, AluPerBaseUrlFlurlClientFactory>();
+builder.Services.AddScoped<IFlurlClient>(s => {
+  var flurlFac = s.GetRequiredService<IFlurlClientFactory>();
+  return flurlFac.Get(new Uri(builder.HostEnvironment.BaseAddress));
+});
+builder.Services.AddScoped(sp =>
+{
+  return new HttpClient() { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
+});
+builder.Services.AddScoped<IWeatherForecastClient, HttpWeatherForecastClient>();
+builder.Services.AddScoped<IWeatherForecastClient, FlurlWeatherForecastClient>();
+
+await builder.Build().RunAsync();
