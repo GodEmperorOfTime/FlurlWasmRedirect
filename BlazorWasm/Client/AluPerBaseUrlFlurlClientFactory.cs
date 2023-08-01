7676d394649957537bcb5286ffb4c9f15ee21672
Jan Janoušek <janousek@ocel.cz>
2023-08-01 11:38:13 +0200

    Separation of ClientFactory

---
+++ BlazorWasm/Client/AluPerBaseUrlFlurlClientFactory.cs
@@ -0,0 +1,17 @@
+﻿using Flurl;
+using Flurl.Http;
+using Flurl.Http.Configuration;
+
+namespace BlazorWasm.Client;
+
+public class AluPerBaseUrlFlurlClientFactory : PerBaseUrlFlurlClientFactory
+{
+  protected override IFlurlClient Create(Url url)
+  {
+    return base
+    .Create(url)
+    //.AllowHttpStatus(System.Net.HttpStatusCode.Found)
+    .WithAutoRedirect(true)          
+    ;
+  }
+}
--- BlazorWasm/Client/IWeatherForecastClient.cs
+++ BlazorWasm/Client/IWeatherForecastClient.cs
@@ -1,7 +1,5 @@
 ﻿using BlazorWasm.Shared;
-using Flurl;
 using Flurl.Http;
-using Flurl.Http.Configuration;
 using System.Net.Http.Json;
 
 namespace BlazorWasm.Client;
@@ -9,62 +7,49 @@ namespace BlazorWasm.Client;
 public interface IWeatherForecastClient
 {
 
-    string Name { get; }
+  string Name { get; }
 
-    Task<WeatherForecast[]> GetForecastsAsync();
+  Task<WeatherForecast[]> GetForecastsAsync();
 
 }
 
 class HttpWeatherForecastClient : IWeatherForecastClient
 {
 
-    public string Name => "HttpClient";
+  public string Name => "HttpClient";
 
-    readonly HttpClient httpClient;
+  readonly HttpClient httpClient;
 
-    public HttpWeatherForecastClient(HttpClient httpClient)
-    {
-        this.httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
-    }
+  public HttpWeatherForecastClient(HttpClient httpClient)
+  {
+    this.httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+  }
 
-    public async Task<WeatherForecast[]> GetForecastsAsync()
-    {
-        return await httpClient.GetFromJsonAsync<WeatherForecast[]>("WeatherForecast")
-          ?? Array.Empty<WeatherForecast>();
-    }
+  public async Task<WeatherForecast[]> GetForecastsAsync()
+  {
+    return await httpClient.GetFromJsonAsync<WeatherForecast[]>("WeatherForecast")
+      ?? Array.Empty<WeatherForecast>();
+  }
 }
 
 class FlurlWeatherForecastClient : IWeatherForecastClient
 {
 
-    readonly IFlurlClient flurlClient;
+  readonly IFlurlClient flurlClient;
 
-    public string Name => "FlurlClient";
+  public string Name => "FlurlClient";
 
-    public FlurlWeatherForecastClient(IFlurlClient flurlClient)
-    {
-        this.flurlClient = flurlClient ?? throw new ArgumentNullException(nameof(flurlClient));
-    }
+  public FlurlWeatherForecastClient(IFlurlClient flurlClient)
+  {
+    this.flurlClient = flurlClient ?? throw new ArgumentNullException(nameof(flurlClient));
+  }
 
-    public async Task<WeatherForecast[]> GetForecastsAsync()
-    {
-        var response = await flurlClient.Request("WeatherForecast").GetAsync();
-        Console.WriteLine($"{response.StatusCode}: {response.ResponseMessage}");
-        return await response.GetJsonAsync<WeatherForecast[]>()
-          ?? Array.Empty<WeatherForecast>();
-    }
+  public async Task<WeatherForecast[]> GetForecastsAsync()
+  {
+    var response = await flurlClient.Request("WeatherForecast").GetAsync();
+    Console.WriteLine($"{response.StatusCode}: {response.ResponseMessage}");
+    return await response.GetJsonAsync<WeatherForecast[]>()
+      ?? Array.Empty<WeatherForecast>();
+  }
 
-}
-
-public class AluPerBaseUrlFlurlClientFactory : PerBaseUrlFlurlClientFactory
-{
-
-    protected override IFlurlClient Create(Url url)
-    {
-        return base
-          .Create(url)
-          //.AllowHttpStatus(System.Net.HttpStatusCode.Found)
-          .WithAutoRedirect(true)
-          ;
-    }
 }
